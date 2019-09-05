c4ab1f893004885aac4d5062b1db845143a0b041
Luis Eduardo Castillo Elias <damsa.lecastillo@damsa.com.mx>
2019-09-05 11:02:23 -0500

    primer commit

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context){
+            _context = context;
+            if(_context.TodoItems.Count() == 0){
+                _context.TodoItems.Add(new TodoItem { Name = "Fuego Number 1"});
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems() {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id) {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if(todoItem == null){
+                return NotFound();
+            }
+            return todoItem;
+        }
+
+        // POST : api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item) {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.id}, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem (long id, TodoItem item) {
+            if(id != item.id) {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id) {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null) {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+        
+        return NoContent()
+        }
+    }
+}
+
---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Models
+{
+    public class TodoItem{
+        public long id {set; get;}
+        public string Name {set; get;}
+        public bool isComplete {set; get;}
+    }    
+}
